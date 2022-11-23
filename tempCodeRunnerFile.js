4e6da1f700ae402d0e2a233e67b15ef5f46fa4ab
julsiam <julsestorco031602@gmail.com>
2022-11-23 10:16:11 +0800

    edited tempCOde

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,36 +1,3 @@
-
-                rl.question('Input must be row column separated with space. Vehicle Location: ', function (location) {
-                    let strLoc = location.trim().split(' ')
-
-                    if (strLoc.length >= 2) {
-                        let row = strLoc[0]
-                        let col = strLoc[1]
-                        parking.unpark(row, col)
-                        console.log('Vehicle Unparked! Drive Safely! The life you save might be yours!\n')
-
-                        rl.prompt()
-                    } else {
-                        console.log('No Vehicle Parked in the area!')
-
-                    }
-                })
-            break
-
-        case 'X':
-        case 'x':
-            rl.close()
-            break
-
-        default:
-            break;
-    }
-
-    rl.prompt();
-
-
-}).on('close', () => {
-
-    console.log('Drive Safely! The life you save might be yours!');
-    process.exit(0);
-
-});
+console.log("\n\t\t********************************************************\n")
+console.log("\t\t          Welcome to the XYZ Parking System               ")
+console.log("\n\t\t********************************************************")
